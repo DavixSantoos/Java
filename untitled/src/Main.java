2f56bb8e7239bece370e2bc64e42a7405cb71478
David <David>
2026-02-17 17:36:31 -0300

    Primeiro Commmit Java

---
+++ untitled/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+    {
+        public static void main(String[] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Olá,"+nome+"! Você tem "+ idade+" anos. ");
+
+            scanner.close();
+
+
+
+        }
+
+    }
